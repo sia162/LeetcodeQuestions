5b6155a6cb5a17f184d53732ffd9d2ef61be3c4e
sia162 <56021920+sia162@users.noreply.github.com>
2022-01-13 21:48:33 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (68.96%) - LeetHub

---
+++ 917-reverse-only-letters/917-reverse-only-letters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        
+        
+     // ?6C40E
+     // i    j
+        
+        for(int i=0,j=s.length()-1;i<j;){
+            
+            if(i<s.length() && !isalpha(s[i])) i++;
+            if(j>=0 && !isalpha(s[j])) j--;
+            
+            if(isalpha(s[i]) && isalpha(s[j])) {swap(s[i],s[j]); i++;j--;};   
+        }
+        
+        return s;
+    }
+};
