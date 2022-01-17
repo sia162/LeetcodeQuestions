720bee3a72dded81f74fb3e84a3c96c09b8015b1
sia162 <56021920+sia162@users.noreply.github.com>
2022-01-17 15:02:50 +0530

    Time: 15 ms (49.29%), Space: 9.8 MB (43.29%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -3,24 +3,13 @@ public:
     void duplicateZeros(vector<int>& arr) {
         
         // 1 0 2 3 0 4 5 0
-            
+           // i 
+        int n = arr.size();
+        int j = n + count(arr.begin(),arr.end(),0);
         
-        vector<int> ans;
-        int i=0,n=arr.size();
-        
-        
-        while(i<n){
-            if(arr[i]!=0){
-                ans.push_back(arr[i]);
-            }else{
-                ans.push_back(0);
-                ans.push_back(0);
-            }
-            i++;
-        }
-        
-        for(int i=0;i<n;i++){
-            arr[i] = ans[i];
+        for(int i=n-1;i>=0;i--){
+            if(--j < n) arr[j]=arr[i];
+            if(arr[i]==0 && --j < n) arr[j] = 0;
         }
     }
 };
