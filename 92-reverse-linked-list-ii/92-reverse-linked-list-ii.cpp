760ef70714d3f2d0d5b1781e1c61b995c2b21dcf
sia162 <56021920+sia162@users.noreply.github.com>
2022-01-19 14:17:45 +0530

    Time: 3 ms (62.60%), Space: 7.5 MB (62.00%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* reverseIt(ListNode* head,ListNode* nullthing){
+        if(head == nullthing || head->next == nullthing) return head;
+        
+        ListNode* newhead = reverseIt(head->next,nullthing);
+        ListNode* headnext = head->next;
+        headnext->next = head;
+        head->next = nullthing;
+        return newhead;
+    }
+    
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        
+        if(head==NULL || head->next == NULL || right == left) return head;
+        
+        ListNode* temp = head;
+        
+        ListNode* rightpointer=NULL,*leftpointer=NULL;
+        int i=1;
+        while(temp!=NULL) {
+            if(i == left){
+                leftpointer = temp;
+            }
+             if(i == right){
+                rightpointer = temp;
+                 // break;
+            }
+            temp=temp->next;
+            i++;
+        }
+        
+        
+        ListNode* nullthing;
+        if(rightpointer == NULL && leftpointer==NULL){
+            leftpointer = head;
+            nullthing = NULL;
+        }else if(leftpointer == NULL){
+            leftpointer = head;
+            nullthing = rightpointer->next;
+        }else if(rightpointer == NULL){
+            nullthing = NULL;
+        }else{
+            nullthing = rightpointer->next;
+        }
+            
+        ListNode* leftnode = leftpointer;
+        ListNode* newheadofreversed = reverseIt(leftpointer,nullthing);
+        
+        if(leftnode!=head)
+           { ListNode* temphead = head;
+                while(temphead!=leftnode){
+                    if(temphead->next == leftnode)
+                        {temphead->next = newheadofreversed;
+                        break;}
+
+                    temphead = temphead->next;
+                }
+                return head;
+            }
+        return newheadofreversed;
+    }
+};
