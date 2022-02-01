fcfe7a16824ee26b14fcfb5b367b55dc369b7369
sia162 <56021920+sia162@users.noreply.github.com>
2022-02-01 16:32:47 +0530

    Time: 42 ms (54.55%), Space: 14.3 MB (32.04%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -6,9 +6,17 @@ public:
         
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                pq.push(matrix[i][j]);
                 
-                if(pq.size()>k) pq.pop();
+                if(k==0){
+                    if(matrix[i][j] < pq.top()){
+                        pq.pop();
+                        pq.push(matrix[i][j]);
+                    }
+                    
+                }else {
+                    pq.push(matrix[i][j]);
+                    k--;
+                }
             }
         }
         
