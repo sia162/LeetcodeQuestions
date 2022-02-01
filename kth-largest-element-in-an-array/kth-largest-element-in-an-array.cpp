69e197a7e95d21a5582ca6ec91cac98788001440
sia162 <56021920+sia162@users.noreply.github.com>
2022-02-01 16:20:18 +0530

    Time: 20 ms, Memory: 10.2 MB - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,15 +4,8 @@ public:
         priority_queue <int, vector<int>, greater<int>> pq; 
         
         for(int i=0;i<nums.size();i++){
-            if(k==0){
-                if(nums[i]>pq.top()){
-                    pq.pop();
-                    pq.push(nums[i]);
-                }
-            }else{
-                pq.push(nums[i]);
-                k--;
-            }
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
         }
         
         return pq.top();
