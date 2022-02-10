26f3dea77a9d03d3d24f7e40cf8b4714a0977d3d
sia162 <56021920+sia162@users.noreply.github.com>
2022-02-10 16:05:12 +0530

    Time: 33 ms (50.61%), Space: 23.9 MB (92.88%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans;
-        vector <int> p;
-        vector<int> s(nums.size());
+        vector<int> ans(nums.size(),1);
+        int left = 1;
+        int right = 1;
+        int n=nums.size();
         
-        int pro=1;
         for(int i=0;i<nums.size();i++){
-            pro = pro*nums[i];
-            p.push_back(pro);
+            ans[i] = ans[i]*left;
+            left = left*nums[i];
+            
+            ans[n-i-1] *=right;
+            right = right*nums[n-i-1];
         }
         
-        pro=1;
-        for(int i=nums.size()-1;i>=0;i--){
-            pro = pro*nums[i];
-            s[i]=pro;
-        }
+//         vector <int> p;
+//         vector<int> s(nums.size());
         
-        for(int k=0;k<nums.size();k++){
-            if(k == 0) ans.push_back(s[k+1]);
-            else if(k == nums.size()-1) ans.push_back(p[k-1]);
-            else{
-                int i=k-1;
-                int j=k+1;
-                ans.push_back(p[i]*s[j]);
-            }
-        }
+//         int pro=1;
+//         for(int i=0;i<nums.size();i++){
+//             pro = pro*nums[i];
+//             p.push_back(pro);
+//         }
+//         pro=1;
+//         for(int i=nums.size()-1;i>=0;i--){
+//             pro = pro*nums[i];
+//             s[i]=pro;
+//         }
+        
+//         for(int k=0;k<nums.size();k++){
+//             if(k == 0) ans.push_back(s[k+1]);
+//             else if(k == nums.size()-1) ans.push_back(p[k-1]);
+//             else{
+//                 int i=k-1;
+//                 int j=k+1;
+//                 ans.push_back(p[i]*s[j]);
+//             }
+//         }
         return ans;
     }
 };
