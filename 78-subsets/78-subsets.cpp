afdfcdc5047b97ed265d878b7b3ea45399545261
sia162 <56021920+sia162@users.noreply.github.com>
2022-02-13 17:47:22 +0530

    Time: 8 ms (20.39%), Space: 17.1 MB (8.23%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    
+    void uniqueSubsets(vector<int> ip , vector<int> op){
+        
+        if(ip.size()==0){
+            ans.push_back(op);
+            return;
+        }
+        
+        vector<int> op1 = op;
+        vector<int> op2 = op;
+        
+        op2.push_back(ip[0]);
+        ip.erase(ip.begin() + 0);
+        
+        uniqueSubsets(ip,op1);
+        uniqueSubsets(ip,op2);
+        
+        return;
+    }
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<int> op;
+        uniqueSubsets(nums,op);
+        
+        return ans;
+        
+    }
+};
