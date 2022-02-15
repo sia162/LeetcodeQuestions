903034bc347976139f529840ee883da376d24b79
sia162 <56021920+sia162@users.noreply.github.com>
2022-02-15 18:56:02 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (75.34%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -12,36 +12,50 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         
-        if(head==NULL || head->next == NULL && n>=1) return NULL;
-        if(head->next == NULL && n<1) return head;
+        ListNode* slow = head;
+        ListNode* fast = head;
         
-        ListNode* temp = head;
-        int size = 0;
-        while(temp!=NULL){
-            temp = temp->next;
-            size++;
+        while(n--) fast = fast->next;
+        if(!fast) return head->next;
+        
+        while(fast->next){
+            slow=slow->next;
+            fast=fast->next;
         }
         
-        if(size == n){
-            return head->next;
-        }
+        slow->next = slow->next->next;
+        return head;
+        
+//         if(head==NULL || head->next == NULL && n>=1) return NULL;
+//         if(head->next == NULL && n<1) return head;
+        
+//         ListNode* temp = head;
+//         int size = 0;
+//         while(temp!=NULL){
+//             temp = temp->next;
+//             size++;
+//         }
+        
+//         if(size == n){
+//             return head->next;
+//         }
         
         
         
-        // cout<<size<<" ";
-        int i = 1;
-        temp = head;
-        while(i<size-n){   
-            temp = temp->next;
-            i++;
-        }
+//         // cout<<size<<" ";
+//         int i = 1;
+//         temp = head;
+//         while(i<size-n){   
+//             temp = temp->next;
+//             i++;
+//         }
         
-        if(n==1)
-            temp->next=NULL;
-        else
-            temp->next = temp->next->next;
+//         if(n==1)
+//             temp->next=NULL;
+//         else
+//             temp->next = temp->next->next;
           
 
-        return head;
+//         return head;
     }
 };
