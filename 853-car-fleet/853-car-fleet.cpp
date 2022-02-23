b3cd1e1f3c1c0ebef6d6e16d860cfc07d4392883
sia162 <56021920+sia162@users.noreply.github.com>
2022-02-23 22:58:24 +0530

    Time: 633 ms (8.08%), Space: 96.7 MB (13.31%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int n = position.size();
-        vector<pair<float,float>> vm;
+//         int n = position.size();
+//         vector<pair<float,float>> vm;
         
-        for(int i=0;i<n;i++) vm.push_back({position[i],speed[i]});
-        sort(vm.begin(),vm.end(),std::greater<>());
+//         for(int i=0;i<n;i++) vm.push_back({position[i],speed[i]});
+//         sort(vm.begin(),vm.end(),std::greater<>());
         
-        int fleet = n;
-        float SlowestTimeToFinish = 0;
-        for(const auto [Pos, Speed]: vm) {
-            const float ttf = (target - Pos) / Speed;
-            ttf > SlowestTimeToFinish ? SlowestTimeToFinish = ttf : --fleet;
-        }
-
-        return fleet;
+//         int fleet = n;
+//         float SlowestTimeToFinish = 0;
+//         for(const auto [Pos, Speed]: vm) {
+//             const float ttf = (target - Pos) / Speed;
+//             ttf > SlowestTimeToFinish ? SlowestTimeToFinish = ttf : --fleet;
+//         }
+//         return fleet;
+        
+        map<int, double> m;
+        for (int i = 0; i < position.size(); i++) 
+            m[-position[i]] = (double)(target - position[i]) / speed[i];
+        
+        int res = 0; double cur = 0;
+        for (auto it : m) if (it.second > cur) cur = it.second, res++;
+        return res;
     }
 };
