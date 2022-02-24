9078a4f3f9445a367b0f2f36733ba0739f4fdfff
sia162 <56021920+sia162@users.noreply.github.com>
2022-02-24 17:00:33 +0530

    Time: 20 ms (52.57%), Space: 13.2 MB (54.98%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-        vector<pair<int,int>> v;
+        int r = matrix.size();
+        int c = matrix[0].size();
+        bool i0flag = false;
         
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-        
+        for(int i=0 ; i<r ; i++){
+             //setting just flags
+            if(matrix[i][0] == 0) i0flag = true;
+            
+            for(int j=1 ; j<c ; j++){
                 if(matrix[i][j]==0){
-                    v.push_back({i,j});
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for(auto [i,j]: v){
-            int pr=i; int pc=0;
-            int qc=j; int qr=0;
-                    
-            while(pc<matrix[0].size())
-               { matrix[pr][pc] = 0; pc++;}
-            while(qr<matrix.size())
-               { matrix[qr][qc] = 0; qr++;}
+        for(int i=r-1;i>=0;i--){
+           for(int j=c-1;j>=1;j--){
+               if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                   matrix[i][j] = 0;
+           } 
+            if(i0flag) matrix[i][0] = 0;
         }
+        
     }
 };
 
