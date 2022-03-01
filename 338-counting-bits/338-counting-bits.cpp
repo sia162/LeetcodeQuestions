12cd85cffd72a4eeea2ae780b052ae08015f8d20
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-03-01 13:25:48 +0530

    Time: 13 ms (20.03%), Space: 8.5 MB (34.00%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     
-    int binary(int n){
-        int count = 0;
+//     int binary(int n){
+//         int count = 0;
         
-        while(n!=0){
-            if(n%2 == 1)
-            {
-                count++;
-            }
-            n=n/2;
-        }
+//         while(n!=0){
+//             if(n%2 == 1)
+//             {
+//                 count++;
+//             }
+//             n=n/2;
+//         }
         
-        return count;
-    }
+//         return count;
+//     }
     
     
     vector<int> countBits(int n) {
         vector<int> ans;
-        for(int i=0;i<=n;i++)
+        
+        ans.push_back(0);
+        for(int i=1;i<=n;i++)
         {
-            int val = binary(i);
+            int val = __builtin_popcount(i);
             ans.push_back(val);
         }
         
