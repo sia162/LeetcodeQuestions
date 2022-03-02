f90c420d9c09838563b81cf5a1df77906ee12388
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-03-02 11:28:58 +0530

    Time: 2 ms (52.38%), Space: 6.3 MB (72.95%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        if(!s.length()) return true;
+        
+        int k = s.length();
+        
+        for(int i=0,j=0;i<s.length() && j<t.length();){
+            if(s[i]==t[j]){
+                i++;
+                k--;
+            }
+            
+            j++;
+        }
+        
+        if(k)
+            return false;
+            
+        return true;
+        
+    }
+};
