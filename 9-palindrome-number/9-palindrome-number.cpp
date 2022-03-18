d75514bebd2fa234f5576c5dd2af19aab1b7087e
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-03-18 22:18:51 +0530

    Time: 4 ms (97.64%), Space: 5.7 MB (91.59%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        
+        for(int i=0,j=s.length()-1;i<=j;i++,j--){
+            if(s[i]!=s[j]) return false;
+        }
+        return true;
+    }
+};
