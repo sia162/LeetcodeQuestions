88eabbfbbee0c7d34a330310b260189c069e0006
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-03-19 16:10:30 +0530

    Time: 45 ms (21.22%), Space: 12.4 MB (16.03%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    string minRemoveToMakeValid(string s) {
-//         stack<pair<char,int>> st;
-//         int n = s.length();
-        
-//         for(int i=n-1;i>=0;i--){
-//             if(s[i]==')') st.push({s[i],i});     
-//             if(s[i]=='(')
-//             st.size() and st.top().first==')'? st.pop(): st.push({s[i],i});  
-//         }
-        
-        
-//         string answer=""; answer.reserve(n);
-//         for(int i=0;i<s.length();i++){
-//             if(st.size() && st.top().second == i) st.pop();
-//             else answer = answer + s[i];
-//         }
-        
-//         return answer;
-        
-        
+    string minRemoveToMakeValid(string s) {        
         int n= s.size();
         stack<pair<char,int>> st; 
         for(int i=n-1;i>=0;i--){
