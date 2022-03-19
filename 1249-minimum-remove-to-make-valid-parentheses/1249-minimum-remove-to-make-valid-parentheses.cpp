25797917a5484a54753caa9e814e89528c7405e6
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-03-19 15:42:51 +0530

    Time: 30 ms (52.99%), Space: 11.6 MB (51.36%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        // if(s.length()<=1) return "";
-      int count = 0;
+        stack<int> st;
         
         for(int i=0;i<s.length();i++){
             if(s[i] == ')'){
-                if(count == 0){
-                    s[i] = '#';
+                if(!st.empty() && s[st.top()] == '('){
+                    st.pop();
                 }else{
-                    count--;
+                    s[i] = '#';
                 }
-            }else if(s[i] == '(') count++;
+            }else if(s[i] == '('){
+                 st.push(i);
+            }
         }
         
-        
-        count = 0;
-        
-          for(int i=s.length()-1;i>=0;i--){
-            if(s[i] == '('){
-                if(count == 0){
-                    s[i] = '#';
-                }else{
-                    count--;
-                }
-            }else if(s[i] == ')') count++;
+        while(!st.empty()){
+            s[st.top()] = '#';
+            st.pop();
         }
         
         string answer;
