e241a780e713a562d668c7a72ea87f0cb084cfd0
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-03-23 15:08:47 +0530

    Time: 0 ms (100.00%), Space: 10 MB (46.22%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -14,8 +14,8 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL || q==NULL) return (p==q);
         
-        return isSameTree(p->left,q->left)
-        && p->val == q->val
+        return p->val == q->val
+        && isSameTree(p->left,q->left)
         && isSameTree(p->right,q->right);
          
     }
