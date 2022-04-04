c62ed33d7f39d8192e879d9cf3dc29a1bcbad07f
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-04-05 00:41:22 +0530

    Time: 4 ms (71.18%), Space: 13.3 MB (97.56%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -11,13 +11,24 @@
  */
 class Solution {
 public:
-    int sum = 0;
     
-    int sumOfLeftLeaves(TreeNode* root , bool leftSide=false) {
-        if(root == NULL) return 0;
-        if(root->left == NULL && root->right == NULL) return leftSide ? root->val : 0;
+    void leftsum(TreeNode* root,int& sum, int direction){
         
-        return sumOfLeftLeaves(root->left,true) + sumOfLeftLeaves(root->right,false);
+        if(direction && root->left == NULL && root->right == NULL) {sum+=root->val; return;}
+        
+        if(root->left) leftsum(root->left,sum,1);
+        if(root->right) leftsum(root->right,sum,0);
+        
+    }
+    
+    int sumOfLeftLeaves(TreeNode* root) {
+        
+        if(!root) return 0;
+        int sum = 0;
+        int direction = 0; //1->left and 0->right
+        
+        leftsum(root,sum,direction);
+        return sum;
         
     }
 };
