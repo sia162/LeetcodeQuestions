433626fe2838386dd5588e050a878cad44f1fca9
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-05-23 19:00:31 +0530

    Time: 7 ms (10.72%), Space: 9.6 MB (13.31%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -22,24 +22,12 @@ public:
 //     }
     
     vector<int> inorderTraversal(TreeNode* root) {
+        if(root == NULL) return {};
         
-        stack<TreeNode*> st;
-        TreeNode* temp = root;
-        
-        while(true){
-            if(temp!=NULL){
-                st.push(temp);
-                temp = temp->left;
-            }else{
-                
-                if(st.empty()) break;
-                temp = st.top();
-                st.pop();
-                inordertraverse.push_back(temp->val);
-                temp = temp->right;  
-            }
-        }
-            
+        inorderTraversal(root->left);
+        inordertraverse.push_back(root->val);
+        inorderTraversal(root->right);
+       
         return inordertraverse;
     }
 };
