c3677495aaefb46d4d4dfa68304639dbc51332e2
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-05-25 18:20:05 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (99.06%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,29 +12,30 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
         if(root==NULL) return {};
-        
-        
+        vector<vector<int>> ans;
         queue<TreeNode*> q;
         q.push(root);
         
-        int direction = 0; //rightToLeft = 1 and leftToRight = 0
+        int direction = 0;
+        // 0 -> right
+        // 1 -> left
+        
         while(!q.empty()){
             int size = q.size();
             vector<int> temp(size);
             
-            for(int i=0;i<size;i++){
+           for(int i=0;i<size;i++){
                 TreeNode* t = q.front();
-                
+
                 int index = direction? size-1-i : i;
                 temp[index] = t->val;
                 q.pop();
-                
+
                 if(t->left!=NULL) q.push(t->left);
                 if(t->right!=NULL) q.push(t->right);
-            
-            }
+
+			}
             
             ans.push_back(temp);
             direction = !direction;
@@ -43,4 +44,4 @@ public:
         return ans;
         
     }
-}; 
+};
