ece962df12343552f91fe5c28208dcff43114066
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-06-10 14:17:32 +0530

    Time: 43 ms (29.14%), Space: 8.9 MB (38.67%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,16 +4,23 @@ public:
         if(s.length() == 1) return 1;
         
         int maxlen = 0;
-        unordered_set<char> st;
+        unordered_map<char,int> m;
         
-        for(int i=0,j=0;j<s.length();){
-            if(st.find(s[j]) == st.end()){
-                st.insert(s[j++]);
-                maxlen = max(maxlen,j-i);
+        for(int i=0,j=0;j<s.length();j++){
+            if(m.find(s[j]) == m.end() || m[s[j]] == 0){
+                m[s[j]]++;
             }else{
-                st.erase(s[i]);
+                int l = m.size();
+                maxlen = max(maxlen,l);
+                while(i<s.length() && s[i]!=s[j]){
+                    m.erase(s[i]);
+                    i++;
+                }
                 i++;
             }
+            
+            int l = m.size();
+            maxlen = max(maxlen,l);
         }
         
         return maxlen; 
