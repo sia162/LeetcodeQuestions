773ff7867cda2ba52d119a681e267b71a19a16aa
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-06-27 19:39:27 +0530

    Time: 32 ms (84.21%), Space: 13.6 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int minPartitions(string n) {
         
-        int ans = 0;
-        for(int i=0;i<n.length();i++){
-            if(n[i] - '0' > ans) ans = n[i] - '0';
-        }
+//         int ans = 0;
+//         for(int i=0;i<n.length();i++){
+//             if(n[i] - '0' > ans) ans = n[i] - '0';
+//         }
         
-        return ans;
+//         return ans;
+        
+       return *max_element(begin(n), end(n)) - '0';
     }
 };
