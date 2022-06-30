78401cb4163c6787892ee16ec66ed4fc69b14905
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-06-30 22:35:50 +0530

    Time: 22 ms (23.04%), Space: 10.8 MB (92.09%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -2,40 +2,14 @@ class Solution {
 public:
     int minMoves2(vector<int>& nums) {
         
-        sort(nums.begin(),nums.end());
+       int n = nums.size(), diffans = 0;
+        sort(nums.begin(), nums.end());
         
-        int mid1=-1,mid2=-1;
-        if(nums.size()%2 == 0){
-            mid1 = nums.size()/2;
-            mid2 = nums.size()/2 -1;
-        }else{
-            mid1 = (nums.size() - 1)/2;
+        int median = nums[n/2];
+        
+        for(int i=0; i<n; i++){
+            diffans += abs(nums[i] - median);
         }
-            
-        
-        int sum1=0,sum2=0;
-        for(int i=0;i<nums.size();i++){
-            if(mid1!=-1){
-                sum1 = sum1 + abs(nums[i]-nums[mid1]); 
-            }
-            
-            if(mid2!=-1){
-                sum2 = sum2 + abs(nums[i]-nums[mid2]); 
-            }
-        }
-        
-        int ans;
-   
-        // cout<<sum1<<" "<<sum2;
-        
-        if(mid2!=-1){
-            ans = sum2;
-        }else if(mid1!=-1){
-            ans = sum1;
-        }else{
-            ans = min(sum1,sum2);
-        }
-        
-        return ans;
+        return diffans;
     }
 };
