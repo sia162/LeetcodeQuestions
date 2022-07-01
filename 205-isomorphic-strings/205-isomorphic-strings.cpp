ced167ec0d954a6bcc97e8893769c82227cb6972
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-07-01 14:47:12 +0530

    Time: 16 ms (24.66%), Space: 7 MB (46.45%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -3,31 +3,26 @@ public:
     bool isIsomorphic(string s, string t) {
         if(s.length()!=t.length()) return false;
         
-        unordered_map<char,char> m;
-        unordered_map<char,char> n;
-        int ans1 = true,ans2 = true;
+        unordered_map<char,int> m;
+        unordered_map<char,int> n;
         
         for(int i=0;i<s.length();i++){
-            if(m.find(s[i]) == m.end()) m[s[i]] = t[i];
-            else {
-                if(m[s[i]]!=t[i]){
-                    ans1 = false;
-                    break;
-                }
-            }
+            if(m.find(s[i]) == m.end()) m[s[i]] = i;
+            s[i] = m[s[i]] + '0';
+            
+            if(n.find(t[i]) == n.end()) n[t[i]] = i;
+            t[i] = n[t[i]] + '0';
         }
         
-        for(int i=0;i<s.length();i++){
-            if(n.find(t[i]) == n.end()) n[t[i]] = s[i];
-            else {
-                if(n[t[i]]!=s[i]){
-                    ans2 = false;
-                    break;
-                }
-            }
-        }
+        cout<<s<<" "<<t;
+
         
-        return ans1 && ans2;
+        // int i = 10;
+        // char a = i+'0';
+        // cout<<a;
+        
+       return s == t;
+        
         
     }
 };
