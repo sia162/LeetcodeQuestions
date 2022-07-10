0c4f3834c2ce60e05439e4e42bee411724dd77f9
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-07-10 23:26:38 +0530

    Time: 24 ms (83.80%), Space: 23.3 MB (40.56%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    void revStr(vector<char>& s,int l,int r){
+        if(l>=r) return;
+        
+        swap(s[l],s[r]);
+        revStr(s,l+1,r-1);
+    }
+    
     void reverseString(vector<char>& s) {
     
         // reverse(s.begin(), s.end());
         // for(int i=0,j=s.size()-1;i<j;i++,j--) swap(s[i],s[j]);
-        int i = 0, j = s.size() - 1;
-        while(i < j){
-            swap(s[i++], s[j--]); 
-        }
         
+        revStr(s,0,s.size()-1);
     }
 };
