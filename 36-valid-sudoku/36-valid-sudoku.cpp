0d4a0416fc9cf1c030b016dcc8a4b410e585a177
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-07-17 11:26:44 +0530

    Time: 27 ms (78.90%), Space: 20.4 MB (23.32%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -2,72 +2,17 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         
-        bool sudo = true;
-        int n = board.size();
-            
-        for(int i=0;i<9;i++){
-            vector<int> check(n+1,0);
-
-            for(int j=0;j<9;j++){
-                if(board[i][j] == '.')
-                    continue;
-                
-                if(isdigit(int(board[i][j]))){
-                    
-                    int index = board[i][j] - '0';
-                    if(check[index] == 1){
-                        return false;
-                    }
-                    
-                    check[index] = 1;
-                }
-               
-            }
-        }
+      vector<set<char>> row(9), col(9), block(9);
         
-   
-        for(int j=0;j<9;j++){
-            vector<int> check(n+1,0);
-        
-            for(int i=0;i<9;i++){
-                if(board[i][j] == '.')
-                    continue;
-                
-                if(isdigit(int(board[i][j]))){
-                    int index = board[i][j] - '0';
-                    if(check[index] == 1){
-                        return false;
-                    }
-                    check[index] = 1;
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] != '.') {
+                    char c = board[i][j];
+                    if(!row[j].insert(c).second or !col[i].insert(c).second or !block[(i/3)*3 + j/3].insert(c).second) 
+						return false;
                 }
             }
         }
-        
-        
-   
-        for(int kc=0;kc<9;kc+=3){
-            for(int kr=0;kr<9;kr+=3){
-                vector<int> check(n+1,0);
-                
-                for(int i=kr;i<kr+3;i++){
-                    for(int j=kc;j<kc+3;j++){
-                        
-                        if(isdigit(int(board[i][j]))){
-                            if(board[i][j] == '.')
-                                continue;
-                            
-                            int index = board[i][j] - '0';
-                            if(check[index] == 1){
-                                return false;
-                                break;
-                            }
-                            check[index] = 1;
-                        }
-                    }
-                }
-            }
-        }
-        
         return true;
     }
 };
