f308f92e5007eb2666e4267f3b0f698f80a85931
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-07-22 04:37:01 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (8.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int solveHere(vector<int> &dp,int n){
-        if(n<=1) return n;
-        
-        if(dp[n]!=-1) return dp[n];
-        
-        return dp[n] = solveHere(dp,n-1) + solveHere(dp,n-2);
-    }
-    
     int fib(int n) {
-        vector<int> dp(n+1,-1);
+        if(n <= 1) return n;
         
-        return solveHere(dp,n);
+        vector<int> dp(n+1,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
     }
 };
