fd4bce4aaf02bf31489fc0d6d1591a7936d97ffc
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-07-22 04:40:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,14 +3,15 @@ public:
     int fib(int n) {
         if(n <= 1) return n;
         
-        vector<int> dp(n+1,-1);
-        dp[0] = 0;
-        dp[1] = 1;
+        int prev1 = 0;
+        int prev2 = 1;
         
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
         }
         
-        return dp[n];
+        return prev2;
     }
 };
