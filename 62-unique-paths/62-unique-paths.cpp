429965966de7e8f08e385f0d182427e87c61534e
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-03 02:06:47 +0530

    Time: 6 ms (12.59%), Space: 6.3 MB (53.26%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
-    
-//     int dp[101][101];
-    
-//     int solveHere(int m,int n,int i,int j){
-//         if(i>m || j>n) return 0;
-//         if(i == m-1 && j == n-1) return 1;
-//         if(dp[i][j]) return dp[i][j];
-//         return dp[i][j] = solveHere(m,n,i+1,j) + solveHere(m,n,i,j+1);
-//     }
-    
     int uniquePaths(int m, int n) {
-        // return solveHere(m,n,0,0);
-        
-        
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        
-        for(int i=0;i<m;i++) dp[i][n-1] = 1;
-        for(int j=0;j<n;j++) dp[m-1][j] = 1;
+        vector<vector<int>> dp(m,vector<int>(n,1));
         
         for(int i=m-2;i>=0;i--){
             for(int j=n-2;j>=0;j--){
