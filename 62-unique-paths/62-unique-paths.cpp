4f80a3e5ee81b95c7172f8f2c8e2c2cd77268a2c
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-07 20:05:41 +0530

    Time: 4 ms (28.92%), Space: 6.3 MB (55.22%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,31 +1,29 @@
 class Solution {
-public:
-//     int solveHere(int i,int j,vector<vector<int>> &dp){
-//         if( i<0 || j<0) return 0;
-//         if(i == 0 && j == 0) return 1;
-//         if(dp[i][j]!=-1) return dp[i][j];
-        
-//         int right = solveHere(i,j-1,dp);
-//         int top = solveHere(i-1,j,dp);
-        
-//         return dp[i][j] = right+top;
-//     }
-    
+public:    
     int uniquePaths(int m, int n) {
-     
-        vector<vector<int>> dp(m,vector<int>(n,1));
-        // return solveHere(m-1,n-1,dp);
-
+        vector<int> prev(n,0);
         
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                int r = dp[i][j+1];
-                int b = dp[i+1][j];
+        for(int i=0; i<m; i++){
+            vector<int> temp(n,0);
+            for(int j=0; j<n; j++){
                 
-                dp[i][j] = r+b;
+                if(i==0 && j==0){
+                    temp[j]=1;
+                    continue;
+                }
+
+                int up=0;
+                int left =0;
+
+                if(i>0) up = prev[j];
+                if(j>0) left = temp[j-1];
+
+                temp[j] = up + left;
             }
-        }
         
-        return dp[0][0];
+            prev = temp;
+        }
+    
+        return prev[n-1];
     }
 };  
