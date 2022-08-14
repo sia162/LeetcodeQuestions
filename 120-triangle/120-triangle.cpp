adb19fc5b8745dc9420bcbe466e83de13f21519b
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-15 00:48:04 +0530

    Time: 7 ms (79.55%), Space: 8.8 MB (44.93%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -13,23 +13,26 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int m=triangle.size();
-        vector<vector<int>> dp(m,vector<int>(m,-1));
+        vector<int> front(m,0);
         
         
         for(int i=m-1;i>=0;i--){
+            vector<int> curr(m,0);
+            
             for(int j=i;j>=0;j--){
-                if(i == m-1) dp[i][j] = triangle[i][j];
+                if(i == m-1) curr[j] = triangle[i][j];
                 else{
                     
-                    int b = triangle[i][j] + dp[i+1][j];
-                    int br = triangle[i][j] + dp[i+1][j+1];
+                    int b = triangle[i][j] + front[j];
+                    int br = triangle[i][j] + front[j+1];
 
-                    dp[i][j] = min(b,br);
+                    curr[j] = min(b,br);
                 }
             }
+            
+            front = curr;
         }
         
-        return dp[0][0];
-        // return solvehere(triangle,0,0,m,dp);
+        return front[0]; 
     }
 };
