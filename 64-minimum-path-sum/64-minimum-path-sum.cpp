02100c9bfb1d72a97dd480ea1c75e8d478de517e
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-14 23:41:33 +0530

    Time: 12 ms (73.23%), Space: 10 MB (63.22%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -16,24 +16,27 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        dp[0][0] = grid[0][0];
+     
+        vector<int> prev(n,0);
         
         for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
+            
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[i][j] = grid[0][0];
+                if(i==0 && j==0) curr[j] = grid[0][0];
                 else{
-                    int l = INT_MAX,r=INT_MAX;
-                    if(i>=1) l = grid[i][j] + dp[i-1][j];
-                    if(j>=1) r = grid[i][j] + dp[i][j-1];
                     
-                    dp[i][j] = min(l,r);
+                    int l = INT_MAX,r=INT_MAX;
+                    if(i>=1) l = grid[i][j] + prev[j];
+                    if(j>=1) r = grid[i][j] + curr[j-1];
+                    
+                    curr[j] = min(l,r);
                 }
             }
+            
+            prev = curr;
         }
         
-        return dp[m-1][n-1];
-        
-        // return solveHere(grid,m-1,n-1,dp);
+        return prev[n-1];
     }
 };
