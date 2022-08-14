5c86e4475de12477ac8f6edf0348cc76dfae15b3
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-15 00:12:44 +0530

    Time: 8 ms (67.81%), Space: 9.1 MB (7.98%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    
-    int solve(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp) {
-		if(i == n - 1)
-			return triangle[i][j];
-		if(dp[i][j] != -1)
-			return dp[i][j];
-		int bottom = triangle[i][j] + solve(i+1, j, n, triangle, dp);
-		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle, dp);
-		return dp[i][j] = min(bottom, bottom_right);
-	}
-    
+    int solvehere(vector<vector<int>>& triangle,int i,int j,int m,vector<vector<int>> &dp){
+        if(i == m-1) return triangle[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int b = triangle[i][j] + solvehere(triangle,i+1,j,m,dp);
+        int br = triangle[i][j] + solvehere(triangle,i+1,j+1,m,dp);
+        
+        return dp[i][j] = min(b,br);
+    }
     
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-		vector<vector<int>>dp(n, vector<int>(n, -1));
-		return solve(0, 0, n, triangle, dp);
+        
+        int m=triangle.size();
+        vector<vector<int>> dp(m,vector<int>(m,-1));
+        
+        return solvehere(triangle,0,0,m,dp);
     }
 };
