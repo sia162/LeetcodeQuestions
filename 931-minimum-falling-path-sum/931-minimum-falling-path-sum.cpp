072a8cf6d4cfbd677655c7da7dc5d10f98f3a61d
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-15 15:30:02 +0530

    Time: 23 ms (51.90%), Space: 10.4 MB (25.64%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
-public:
-    int solvehere(vector<vector<int>>& mat,int i,int j,int n,vector<vector<int>> &dp){
-        if (j < 0 || j >= n) return 1e9; // Base Case 1: out of bound
-        if (i == 0) return mat[i][j]; // Base Case 2: end point - 0th row
-        if(dp[i][j] != -1) return dp[i][j];
-         
-        int top = mat[i][j] + solvehere(mat, i - 1, j, n,dp);
-        int top_left = mat[i][j] + solvehere(mat, i - 1, j - 1, n,dp);
-        int top_right = mat[i][j] + solvehere(mat, i - 1, j + 1, n,dp);
-        
-        return dp[i][j] = min(top, min(top_left, top_right));
-    }
-    
+public:    
     int minFallingPathSum(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
         int min_path_sum = 1e9;
         vector<vector<int>> dp(n,vector<int>(n,-1));;
         
-        for (int j = 0; j < n; j++) {
-            min_path_sum = min(min_path_sum, solvehere(matrix, n - 1, j, n,dp));
-        }
+        for(int j=0;j<n;j++) dp[0][j] = matrix[0][j]; //base case
+            
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++) { //j is depend on i for each i j can be all possible values 0,1,2..n-1 
+                
+                int top = 1e9, top_left = 1e9, top_right = 1e9;
+
+                top = matrix[i][j] + dp[i - 1][j];
+                if(j-1>=0) top_left = matrix[i][j] + dp[i - 1][j - 1];
+                if(j+1<n) top_right = matrix[i][j] + dp[i - 1][j + 1];
+
+                dp[i][j] = min(top, min(top_left, top_right));
+                 
+            }
+        } 
         
+        for (int j = 0; j < n; j++) min_path_sum = min(min_path_sum, dp[n - 1][j]);
         return min_path_sum;
     }
 };
