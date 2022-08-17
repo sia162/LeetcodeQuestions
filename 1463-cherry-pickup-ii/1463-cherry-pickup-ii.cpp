172d0fd803c94dbe4f22c0b771b76562725997aa
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-17 18:20:46 +0530

    Time: 317 ms (16.76%), Space: 9.3 MB (96.01%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -29,15 +29,16 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<vector<int>>> dp(n , vector<vector<int>> (m, vector<int> (m,-1)));
+        vector<vector<int>> front(m,vector<int>(m,-1));
+        vector<vector<int>> curr(m,vector<int>(m,-1));
         
+         
         for(int j1=0;j1<m;j1++){
             for(int j2=0;j2<m;j2++){
-                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
-                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+                if(j1 == j2) front[j1][j2] = grid[n-1][j1];
+                else front[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
             }
         }
-        
         
         for(int i=n-2;i>=0;i--){
             for(int j1=0;j1<m;j1++){
@@ -55,7 +56,7 @@ public:
                             }
                             
                             if(j1+dj1 >=0 && j1+dj1<m && j2+dj2 >=0 && j2+dj2<m)
-                                value += dp[i+1][j1+dj1][j2+dj2];
+                                value += front[j1+dj1][j2+dj2];
                             else value += -1e8;
                                 
                                 
@@ -64,13 +65,13 @@ public:
                         }
                     } 
 
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+            
+            front = curr;
         }
         
-        
-        return dp[0][0][m-1];
-     
+        return front[0][m-1];
     }
 }; 
