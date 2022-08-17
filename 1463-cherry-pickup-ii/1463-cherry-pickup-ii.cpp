2c1de4786546565f8410f48fd5e429320d1dbbf4
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-17 18:11:23 +0530

    Time: 394 ms (10.90%), Space: 16.9 MB (39.39%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,37 +1,76 @@
 class Solution {
 public:
-    int solvehere(vector<vector<int>>& g,int i,int j1,int j2,int n,int m,vector<vector<vector<int>>> &dp){
-        if(j1>=n || j2>=n || j1<0 || j2<0) return -1e9; //out of bound
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+//     int solvehere(vector<vector<int>>& g,int i,int j1,int j2,int n,int m,vector<vector<vector<int>>> &dp){
+//         if(j1>=n || j2>=n || j1<0 || j2<0) return -1e9; //out of bound
+//         if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+//         if(i == m-1){ //destination
+//             if(j1==j2) return g[i][j1]; //in case same col
+//             return g[i][j1] + g[i][j2]; // in case different col
+//         }
+//         if(j1 == j2) return g[i][j1];
+//
+//         //9 possible paths;
+//         int maxi = -1e9;
+//         for(int dj1 = -1;dj1 <= 1;dj1++){
+//             for(int dj2 = -1; dj2 <= 1;dj2++){
+//                 if(j1 == j2) maxi = max(maxi, g[i][j1] + solvehere(g,i+1,j1+dj1,j2+dj2,n,m,dp));
+//                 else{
+//                   maxi = max(maxi, g[i][j1] + g[i][j2] + solvehere(g,i+1,j1+dj1,j2+dj2,n,m,dp));  
+//                 }
+//             }
+//         } 
+//
+//         dp[i][j1][j2] = maxi;
+//         return maxi;
+//     }
+    
+    int cherryPickup(vector<vector<int>>& grid) {
         
-        if(i == m-1){ //destination
-            if(j1==j2) return g[i][j1]; //in case same col
-            return g[i][j1] + g[i][j2]; // in case different col
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<vector<int>>> dp(n , vector<vector<int>> (m, vector<int> (m,-1)));
+        
+        for(int j1=0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
         }
-        if(j1 == j2) return g[i][j1];
+        
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    
+                    int maxi = -1e9;
+                    for(int dj1 = -1;dj1 <= 1;dj1++){
+                        for(int dj2 = -1; dj2 <= 1;dj2++){
+                            
+                            int value = 0;
+                            
+                            if(j1 == j2) value = grid[i][j1];
+                            else{
+                              value = grid[i][j1] + grid[i][j2];
+                            }
+                            
+                            if(j1+dj1 >=0 && j1+dj1<m && j2+dj2 >=0 && j2+dj2<m)
+                                value += dp[i+1][j1+dj1][j2+dj2];
+                            else value += -1e8;
+                                
+                                
+                            maxi = max(maxi,value);
+                            
+                        }
+                    } 
 
-        //9 possible paths;
-        int maxi = -1e9;
-        for(int dj1 = -1;dj1 <= 1;dj1++){
-            for(int dj2 = -1; dj2 <= 1;dj2++){
-                if(j1 == j2) maxi = max(maxi, g[i][j1] + solvehere(g,i+1,j1+dj1,j2+dj2,n,m,dp));
-                else{
-                  maxi = max(maxi, g[i][j1] + g[i][j2] + solvehere(g,i+1,j1+dj1,j2+dj2,n,m,dp));  
+                    dp[i][j1][j2] = maxi;
                 }
             }
         }
         
-        dp[i][j1][j2] = maxi;
-        return maxi;
-    }
-    
-    int cherryPickup(vector<vector<int>>& grid) {
         
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        vector<vector<vector<int>>> dp(m , vector<vector<int>> (n, vector<int> (n,-1)));
-        
-        return solvehere(grid,0,0,n-1,n,m,dp);
+        return dp[0][0][m-1];
+     
     }
 }; 
