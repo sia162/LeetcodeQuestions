77819904bcec0deae86dcb65c586caee994d5ff9
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-18 08:56:08 +0530

    Time: 214 ms (10.21%), Space: 53.6 MB (20.55%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        return nums.size()>set<int>(nums.begin(),nums.end()).size();
+        unordered_map<int,int> m;
+        for(auto v:nums) m[v]++;
+        
+        for(auto [val,freq]: m){
+            if(freq>=2) return true;
+        }
+        
+        return false;
     }
 };
