c98a8cef2429424cfcf899ebf5344851de551ba5
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-18 09:10:22 +0530

    Time: 10 ms (73.79%), Space: 7.3 MB (46.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,18 +3,16 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
         
-        unordered_map<char,int> m1;
-        unordered_map<char,int> m2;
-        
+        unordered_map<char,int> m;
         
         for(int i=0;i<s.size();i++){
-            m1[s[i]]++;
-            m2[t[i]]++;
+            m[s[i]]++;
+            m[t[i]]--;
         }
    
         
         for(auto v:s){
-            if(m1[v] != m2[v]) return false;
+            if(m[v] != 0) return false;
         }
         
         return true;
