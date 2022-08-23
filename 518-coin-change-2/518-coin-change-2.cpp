505588e75d88a6ec051db4fb31fff5b36805491e
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-23 16:33:44 +0530

    Time: 64 ms (30.11%), Space: 18.4 MB (49.62%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-      int solvehere(int ind,vector<int>& coins,int tar,vector<vector<int>> &dp){
-        if(ind == 0){
-            if(tar%coins[ind] == 0) return 1;
-            else return 0;
-        }
+//       int solvehere(int ind,vector<int>& coins,int tar,vector<vector<int>> &dp){
+//         if(ind == 0){
+//             if(tar%coins[ind] == 0) return 1;
+//             else return 0;
+//         }
           
-        if(dp[ind][tar] != -1) return dp[ind][tar];
+//         if(dp[ind][tar] != -1) return dp[ind][tar];
         
-        int pick = 0;
-        if(coins[ind] <= tar) pick = solvehere(ind,coins,tar - coins[ind],dp);
-        int notpick = solvehere(ind-1,coins,tar,dp);
+//         int pick = 0;
+//         if(coins[ind] <= tar) pick = solvehere(ind,coins,tar - coins[ind],dp);
+//         int notpick = solvehere(ind-1,coins,tar,dp);
         
-        return dp[ind][tar] = pick + notpick;
-    } 
+//         return dp[ind][tar] = pick + notpick;
+//     } 
     
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         
-        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        return solvehere(n-1,coins,amount,dp);
+        vector<vector<int>> dp(n,vector<int> (amount+1,0));
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0] == 0) dp[0][t] = 1;
+            else dp[0][t] = 0;
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=amount;t++){
+                int pick = 0;
+                if(coins[i] <= t) pick = dp[i][t - coins[i]];
+                int notpick = dp[i-1][t];
+
+                dp[i][t] = pick + notpick;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
 };
