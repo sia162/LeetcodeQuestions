52a88afb03bf0d03fe861642e2967faa90908b5b
Siya Banodha <56021920+sia162@users.noreply.github.com>
2022-08-25 06:05:30 +0530

    Time: 23 ms (89.23%), Space: 18.8 MB (55.84%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,18 +1,58 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        int n = 10;
         
-      vector<set<char>> row(9), col(9), block(9);
+        for(int i=0;i<9;i++){
+            vector<int> check(n,0);
+            
+            for(int j=0;j<9;j++){
+                int index = board[i][j] - '0';
+                
+                if(board[i][j] == '.') continue;
+                if(check[index] == 1) return false;
+                
+                check[index] = 1;
+            }
+        }
         
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j] != '.') {
-                    char c = board[i][j];
-                    if(!row[j].insert(c).second or !col[i].insert(c).second or !block[(i/3)*3 + j/3].insert(c).second) 
-						return false;
+        
+        for(int j=0;j<9;j++){
+            vector<int> check(n,0);
+            
+            for(int i=0;i<9;i++){
+                int index = board[i][j] - '0';
+                
+                if(board[i][j] == '.') continue;
+                if(check[index] == 1) return false;
+                
+                check[index] = 1;
+            }
+        }
+        
+        
+        for(int r=0;r<3;r++){
+            for(int c=0;c<3;c++){
+                int starti = r*3;
+                int endi = starti + 3;
+                
+                int startj = c*3;
+                int endj = startj+3;
+                
+                vector<int> check(n,0);
+                
+                for(int i=starti;i<endi;i++){
+                    for(int j=startj;j<endj;j++){
+                        int index = board[i][j] - '0';
+                        
+                        if(board[i][j] == '.') continue;
+                        if(check[index] == 1) return false;
+                        check[index] = 1;
+                    }
                 }
             }
         }
+        
         return true;
     }
 };
