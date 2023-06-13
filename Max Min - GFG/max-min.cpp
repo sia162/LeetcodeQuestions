3233c1fc2642e03578fe51948f10ff564b6d4706
Siya Banodha <56021920+sia162@users.noreply.github.com>
2023-06-13 19:31:04 +0530

    Added solution - LeetHub

--- Max Min - GFG/max-min.cpp
+++ Max Min - GFG/max-min.cpp
@@ -9,17 +9,64 @@ using namespace std;
 
 class Solution
 {
-   public:
+    public:
+    struct Pair {
+        int min;
+        int max;
+    };
+     
+    struct Pair getMinMax(int arr[], int low, int high)
+    {
+        struct Pair minmax, mml, mmr;
+        int mid;
+     
+        // If there is only one element
+        if (low == high) {
+            minmax.max = arr[low];
+            minmax.min = arr[low];
+            return minmax;
+        }
+     
+        // If there are two elements
+        if (high == low + 1) {
+            if (arr[low] > arr[high]) {
+                minmax.max = arr[low];
+                minmax.min = arr[high];
+            }
+            else {
+                minmax.max = arr[high];
+                minmax.min = arr[low];
+            }
+            return minmax;
+        }
+     
+        // If there are more than 2 elements
+        mid = (low + high) / 2;
+        mml = getMinMax(arr, low, mid);
+        mmr = getMinMax(arr, mid + 1, high);
+     
+        // Compare minimums of two parts
+        if (mml.min < mmr.min)
+            minmax.min = mml.min;
+        else
+            minmax.min = mmr.min;
+     
+        // Compare maximums of two parts
+        if (mml.max > mmr.max)
+            minmax.max = mml.max;
+        else
+            minmax.max = mmr.max;
+     
+        return minmax;
+    }
+   
+   
     int findSum(int A[], int N)
     {
     	//code here.
-    	int max = INT_MIN,min = INT_MAX;
-    	for(int i=0;i<N;i++){
-    	    if(A[i]>max) max = A[i];
-    	    if(A[i]<min) min = A[i];
-    	}
+    	struct Pair minmax = getMinMax(A, 0, N - 1);
     	
-    	return max+min;
+    	return minmax.max+minmax.min;
     }
 
 };
