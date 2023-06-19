df729f41836305f97dbdda022228a317e94e2ad9
Siya Banodha <56021920+sia162@users.noreply.github.com>
2023-06-19 21:47:34 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -7,55 +7,42 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-class Solution {
-private:
-	bool dfsCheck(int node, vector<int> adj[], int vis[],int pathVis[], int check[]) {
-		vis[node] = 1;
-		pathVis[node] = 1;
-		check[node] = 0;
-		// traverse for adjacent nodes
-		for (auto it : adj[node]) {
-			// when the node is not visited
-			if (!vis[it]) {
-			if (dfsCheck(it, adj, vis, pathVis, check) == true) {
-					check[node] = 0;
-					return true;
-				}
 
-			}
-			// if the node has been previously visited
-			// but it has to be visited on the same path
-			else if (pathVis[it]) {
-				check[node] = 0;
-				return true;
+class Solution {
+  public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        // code here
+        vector<int> adjRev[V];
+		int indegree[V] = {0};
+		for (int i = 0; i < V; i++) {
+			// i -> it
+			// it -> i
+			for (auto it : adj[i]) {
+				adjRev[it].push_back(i);
+				indegree[i]++;
 			}
 		}
-		
-		check[node] = 1;
-		pathVis[node] = 0;
-		return false;
-	}
-	
-public:
-	vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
-		int vis[V] = {0};
-		int pathVis[V] = {0};
-		int check[V] = {0};
-		
+		queue<int> q;
 		vector<int> safeNodes;
-		
 		for (int i = 0; i < V; i++) {
-			if (!vis[i]) {
-				dfsCheck(i, adj, vis, pathVis, check);
+			if (indegree[i] == 0) {
+				q.push(i);
 			}
 		}
-		
-		
-		for (int i = 0; i < V; i++) {
-			if (check[i] == 1) safeNodes.push_back(i);
+
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			safeNodes.push_back(node);
+			for (auto it : adjRev[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0) q.push(it);
+			}
 		}
+
+		sort(safeNodes.begin(), safeNodes.end());
 		return safeNodes;
-	}
+    }
 };
 
 
