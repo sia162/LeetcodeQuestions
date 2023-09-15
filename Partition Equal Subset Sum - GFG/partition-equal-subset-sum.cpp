03408c00c4d6f09ffb5ef7fe76bb063183c8c49a
Siya Banodha <56021920+sia162@users.noreply.github.com>
2023-09-15 16:32:49 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,31 +9,47 @@ using namespace std;
 
 class Solution{
 public:
-    int solve(int i,int arr[],int t,vector<vector<int>> &dp){
+    int solve(int i,int t,int arr[],vector<vector<int>> &dp){
         if(t == 0) return 1;
-        if(i<0) return 0;
+        if(i == 0) return t == arr[0];
         
-        if(dp[i][t]!=-1) return dp[i][t];
+        if(dp[i][t] != -1) return dp[i][t];
         
-        int nottake = solve(i-1,arr,t,dp);
-        int take = 0;
-        if(arr[i]<=t) take = solve(i-1,arr,t-arr[i],dp);
+        int pick = 0;
+        if(arr[i]<=t) pick = solve(i-1,t-arr[i],arr,dp);
+        int notpick = solve(i-1,t,arr,dp);
         
-        return dp[i][t] = take || nottake;
+        return dp[i][t] = pick || notpick;
     }
 
     int equalPartition(int N, int arr[])
     {
         // code here
         int s = 0;
-        for(int i=0;i<N;i++) s+=arr[i];
+        for(int i=0;i<N;i++){
+            s+=arr[i];
+        }
         
         if(s%2!=0) return 0;
-        
         int target = s/2;
-        vector<vector<int>> dp(N,vector<int>(target+1,-1));
         
-        return solve(N-1,arr,target,dp);
+        vector<vector<int>> dp(N,vector<int>(target+1,0));
+        
+        for(int i=0;i<N;i++) dp[i][0] = 1;
+        
+        for(int i=1;i<N;i++){
+            for(int t=0;t<=target;t++){
+                int pick = 0;
+                if(arr[i]<=t) pick = dp[i-1][t-arr[i]];
+                int notpick = dp[i-1][t];
+                
+                dp[i][t] = pick || notpick;
+            }
+        }
+        
+        return dp[N-1][target];
+        
+        // return solve(N-1,target,arr,dp);
     }
 };
 
